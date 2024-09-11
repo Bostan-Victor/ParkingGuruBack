2d49858c0af4fbc518acf7d158b2511e574f72b9
Bostan Victor <victorbostan01@gmail.com>
2024-09-12 00:10:41 +0300

    Initialize spring project

---
+++ src/main/java/parking/guru/ParkingGuruApplication.java
@@ -0,0 +1,13 @@
+package parking.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/parking/guru/ParkingGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package parking.guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
