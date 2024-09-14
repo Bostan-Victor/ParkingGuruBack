01ed362cb7e1192f2a929a7d6af1180c105809ad
Dan <114677896+Doschinescu@users.noreply.github.com>
2024-09-15 00:27:49 +0300

    added CRUD operations for vehicle

--- src/main/java/parking/guru/repositories/VehicleRepository.java
+++ src/main/java/parking/guru/repositories/VehicleRepository.java
@@ -2,8 +2,12 @@ package parking.guru.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import parking.guru.models.Vehicle;
+
 import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
     Optional<Vehicle> findByPlateNumber(String plateNumber);
+
+    boolean existsByPlateNumber(String plateNumber);
 }
--- src/main/java/parking/guru/services/VehicleService.java
+++ src/main/java/parking/guru/services/VehicleService.java
@@ -2,14 +2,46 @@ package parking.guru.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import parking.guru.models.Vehicle;
 import parking.guru.repositories.VehicleRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
+
     private final VehicleRepository vehicleRepository;
 
+    @Autowired
     public VehicleService(VehicleRepository vehicleRepository) {
         this.vehicleRepository = vehicleRepository;
     }
-}
 
+    @Transactional
+    public Vehicle saveVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    public Optional<Vehicle> getVehicleById(Long id) {
+        return vehicleRepository.findById(id);
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    public Optional<Vehicle> getVehicleByPlateNumber(String plateNumber) {
+        return vehicleRepository.findByPlateNumber(plateNumber);
+    }
+
+    public boolean hasVehicleWithPlateNumber(String plateNumber) {
+        return vehicleRepository.existsByPlateNumber(plateNumber);
+    }
+
+    @Transactional
+    public void deleteVehicle(Long id) {
+        vehicleRepository.deleteById(id);
+    }
+}
