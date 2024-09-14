176101e2ebda08e4856b8aeb7d64b5ac49a551d9
Dan <114677896+Doschinescu@users.noreply.github.com>
2024-09-14 23:29:16 +0300

    Added CRUM operations for profile

--- src/main/java/parking/guru/services/ProfileService.java
+++ src/main/java/parking/guru/services/ProfileService.java
@@ -1,7 +1,11 @@
 package parking.guru.services;
 
 import org.springframework.stereotype.Service;
+import parking.guru.models.Profile;
 import parking.guru.repositories.ProfileRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfileService {
@@ -9,5 +13,25 @@ public class ProfileService {
 
     public ProfileService(ProfileRepository profileRepository) {
         this.profileRepository = profileRepository;
+    }
+
+    public Profile saveProfile(Profile profile) {
+        return profileRepository.save(profile);
+    }
+
+    public Optional<Profile> getProfileById(Long id) {
+        return profileRepository.findById(id);
+    }
+
+    public List<Profile> getAllProfiles() {
+        return profileRepository.findAll();
+    }
+
+    public void deleteProfile(Long id) {
+        profileRepository.deleteById(id);
+    }
+
+    public Optional<Boolean> isProfileVerified(Long id) {
+        return profileRepository.isUserVerified(id);
     }
 }
