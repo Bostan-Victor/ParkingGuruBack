71f590b459042c382c31dbb56db2cd1042761d95
Dan <114677896+Doschinescu@users.noreply.github.com>
2024-09-17 21:28:21 +0300

    Optimised code by rewriting some classes

--- src/main/java/parking/guru/controllers/UserController.java
+++ src/main/java/parking/guru/controllers/UserController.java
@@ -3,7 +3,10 @@ package parking.guru.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import parking.guru.dtos.CreateUserInput;
+import parking.guru.dtos.UpdateUserInput;
 import parking.guru.models.User;
+import parking.guru.models.enums.Role;
 import parking.guru.services.UserService;
 
 import java.util.List;
@@ -20,10 +23,49 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<User> createOrUpdateUser(@RequestBody User user) {
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserInput input) {
+        User user = new User();
+        user.setEmail(input.getEmail());
+        user.setFirstName(input.getFirstName());
+        user.setLastName(input.getLastName());
+        user.setPhoneNumber(input.getPhoneNumber());
+        user.setUid(input.getUid());
+        user.setRole(Role.valueOf(input.getRole()));
+
         User savedUser = userService.saveUser(user);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UpdateUserInput input) {
+        User user = userService.getUserById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (input.getEmail() != null) {
+            user.setEmail(input.getEmail());
+        }
+        if (input.getFirstName() != null) {
+            user.setFirstName(input.getFirstName());
+        }
+        if (input.getLastName() != null) {
+            user.setLastName(input.getLastName());
+        }
+        if (input.getPhoneNumber() != null) {
+            user.setPhoneNumber(input.getPhoneNumber());
+        }
+        if (input.getRole() != null) {
+            user.setRole(Role.valueOf(input.getRole()));
+        }
+
+        User updatedUser = userService.saveUser(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok(true);
     }
 
     @GetMapping("/{id}")
@@ -47,17 +89,5 @@ public class UserController {
         Optional<User> user = userService.getUserByEmail(email);
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        Optional<User> user = userService.getUserById(id);
-        if (user.isPresent()) {
-            userService.deleteUser(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
     }
 }
