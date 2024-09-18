5caa3b83f3da92c598aad0f287979e70269db76d
Doschinescu <doschinescud27@gmail.com>
2024-09-19 01:39:34 +0300

    Changed VehicleController + fixed some errors

--- src/main/java/parking/guru/controllers/UserQueryResolver.java
+++ src/main/java/parking/guru/controllers/UserQueryResolver.java
@@ -7,25 +7,12 @@ import org.springframework.stereotype.Controller;
 import parking.guru.models.User;
 import parking.guru.services.UserService;
 
-import java.util.List;
-import java.util.Optional;
-
 @Controller
 @RequiredArgsConstructor
 public class UserQueryResolver {
 
     private final UserService userService;
 
-    @QueryMapping
-    public List<User> allUsers() {
-        return userService.getAllUsers();
-    }
-
-    @QueryMapping
-    public User userById(@Argument Long id) {
-        return userService.getUserById(id)
-                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
-    }
     @QueryMapping
     public User userByEmail(@Argument String email) {
         return userService.getUserByEmail(email)
